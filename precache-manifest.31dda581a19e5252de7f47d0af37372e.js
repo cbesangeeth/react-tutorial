a78fdce2d4aa25f19e488da9dea2e6361f82e65d
cbesangeeth <cbe.sangeeth@gmail.com>
2018-12-04 23:28:03 +0530

    Updates

---
+++ precache-manifest.31dda581a19e5252de7f47d0af37372e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "6d203e62ef43227ac1f7",
+    "url": "/react-tutorial/static/js/main.6d203e62.chunk.js"
+  },
+  {
+    "revision": "65ce664db22ebc0891b6",
+    "url": "/react-tutorial/static/js/1.65ce664d.chunk.js"
+  },
+  {
+    "revision": "6d203e62ef43227ac1f7",
+    "url": "/react-tutorial/static/css/main.4c5ee6d0.chunk.css"
+  },
+  {
+    "revision": "ba9f89fbb6d7b1873362579f45a04660",
+    "url": "/react-tutorial/index.html"
+  }
+];
